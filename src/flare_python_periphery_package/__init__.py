5db7b38a28832904bac8fe22c46dacef5860553e
Jošt Smrtnik <jost.smrtnik@aflabs.si>
2024-08-16 08:42:34 +0200

    added name to address, needs removing chain argument

--- src/flare_python_periphery_package/__init__.py
+++ src/flare_python_periphery_package/__init__.py
@@ -1,3 +1,57 @@
 from . import coston, coston2, flare, songbird
+from web3 import Web3
 
-__all__ = ["coston", "coston2", "flare", "songbird"]
+__all__ = [
+    "coston",
+    "coston2",
+    "flare",
+    "songbird",
+    "name_to_abi",
+    "name_to_address",
+    "names_to_addresses",
+]
+
+
+def name_to_abi(name: str, network: str):
+    if network.lower() == "coston":
+        return coston.name_to_abi(name)
+    elif network.lower() == "coston2":
+        return coston2.name_to_abi(name)
+    elif network.lower() == "coston":
+        return flare.name_to_abi(name)
+    elif network.lower() == "coston":
+        return songbird.name_to_abi(name)
+    else:
+        raise KeyError(
+            f"Unsupported network '{network}'. Supported networks are coston, coston2, flare and songbird"
+        )
+
+
+def name_to_address(name: str, network: str, provider: Web3) -> str:
+    if network.lower() == "coston":
+        return coston.name_to_address(name, provider)
+    elif network.lower() == "coston2":
+        return coston2.name_to_address(name, provider)
+    elif network.lower() == "coston":
+        return flare.name_to_address(name, provider)
+    elif network.lower() == "coston":
+        return songbird.name_to_address(name, provider)
+    else:
+        raise KeyError(
+            f"Unsupported network '{network}'. Supported networks are coston, coston2, flare and songbird"
+        )
+
+
+def names_to_addresses(names: list[str], network: str, provider: Web3) -> list[str]:
+    if network.lower() == "coston":
+        return coston.names_to_addresses(names, provider)
+    elif network.lower() == "coston2":
+        return coston2.names_to_addresses(names, provider)
+    elif network.lower() == "coston":
+        return flare.names_to_addresses(names, provider)
+    elif network.lower() == "coston":
+        return songbird.names_to_addresses(names, provider)
+    else:
+        raise KeyError(
+            f"Unsupported network '{network}'. Supported networks are coston, coston2, flare and songbird"
+        )
--- src/flare_python_periphery_package/coston2/mapper.py
+++ src/flare_python_periphery_package/coston2/mapper.py
@@ -1,10 +1,11 @@
 from .abis import _ABIGetter
+from web3 import Web3
 
 flare_contract_registry_address = "0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019"
 
 
-def name_to_address(name: str, provider) -> str: ...  # TODO
-def names_to_addresses(names: list[str], provider) -> list[str]: ...  # TODO
+def name_to_address(name: str, provider: Web3) -> str: ...  # TODO
+def names_to_addresses(names: list[str], provider: Web3) -> list[str]: ...  # TODO
 
 
 def name_to_abi(name: str):
