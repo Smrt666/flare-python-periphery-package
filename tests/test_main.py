6a21dd9bfb92e7940bb108e471268e1b15188f94
Jošt Smrtnik <jost.smrtnik@aflabs.si>
2024-08-21 11:34:06 +0200

    updated tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,6 +9,7 @@ class TestEverythingImports(unittest.TestCase):
     def test_toplevel_import(self):
         self.assertTrue(Web3.is_checksum_address(fpp.FLARE_CONTRACT_REGISTRY_ADDRESS))
         fpp.name_to_abi
+        fpp.interface_to_abi
         fpp.coston
         fpp.coston2
         fpp.flare
@@ -20,6 +21,9 @@ class TestEverythingImports(unittest.TestCase):
 
     def test_chain_import(self):
         for chain in [fpp.coston, fpp.coston2, fpp.flare, fpp.songbird]:
+            chain.interface_to_abi
             chain.name_to_abi
-            chain.abis
+            chain.interface_abis
+            chain.products
             self.assertRaises(KeyError, lambda: chain.name_to_abi("sdfj"))
+            self.assertRaises(KeyError, lambda: chain.interface_to_abi("sdfj"))
